363a276728ca111d3a8b21d72db2a7bdaf8eb8b9
Patrick Doyle <pdoyle@venacorp.com>
2023-01-11 12:02:33 -0500

    Put all flushModes back

--- bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/TestParameters.java
+++ bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/TestParameters.java
@@ -4,6 +4,7 @@ import io.vena.bosk.drivers.mongo.MongoDriverSettings.MongoDriverSettingsBuilder
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
+import static io.vena.bosk.drivers.mongo.MongoDriverSettings.FlushMode.ECHO;
 import static io.vena.bosk.drivers.mongo.MongoDriverSettings.FlushMode.REVISION_FIELD;
 
 public interface TestParameters {
@@ -13,18 +14,18 @@ public interface TestParameters {
 	static Stream<MongoDriverSettingsBuilder> driverSettings() {
 		String prefix = "boskTestDB_" + dbCounter.incrementAndGet();
 		return Stream.of(
-//			MongoDriverSettings.builder()
-//				.database(prefix + "_echo")
-//				.flushMode(ECHO),
+			MongoDriverSettings.builder()
+				.database(prefix + "_echo")
+				.flushMode(ECHO),
 			MongoDriverSettings.builder()
 				.database(prefix + "_rev")
-				.flushMode(REVISION_FIELD)//,
-//			MongoDriverSettings.builder()
-//				.database(prefix + "_slow")
-//				.flushMode(REVISION_FIELD)
-//				.testing(MongoDriverSettings.Testing.builder()
-//					.eventDelayMS(100)
-//					.build())
+				.flushMode(REVISION_FIELD),
+			MongoDriverSettings.builder()
+				.database(prefix + "_slow")
+				.flushMode(REVISION_FIELD)
+				.testing(MongoDriverSettings.Testing.builder()
+					.eventDelayMS(100)
+					.build())
 		);
 	}
 
