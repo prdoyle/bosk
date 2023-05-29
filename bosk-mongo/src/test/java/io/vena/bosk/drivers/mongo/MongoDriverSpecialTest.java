prdoyle/bosk-old

WIP initial outage test

bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/MongoDriverSpecialTest.java
@@ -31,6 +31,7 @@ import java.util.Optional;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import lombok.Value;
+import lombok.var;
 import org.bson.BsonDocument;
 import org.bson.BsonNull;
 import org.bson.BsonString;
@@ -253,6 +254,57 @@ class MongoDriverSpecialTest implements TestParameters {
 			latecomerActual = latecomerBosk.rootReference().value();
 		}
 		assertEquals(expected, latecomerActual);
+	}
+
+	@ParametersByName
+	@DisruptsMongoService
+	void initialOutage_boskRecovers() throws InvalidTypeException, InterruptedException, IOException {
+		// Set up the database contents to be different from initialRoot
+		Bosk<TestEntity> prepBosk = new Bosk<TestEntity>(
+			"Prep bosk",
+			TestEntity.class,
+			bosk -> initialRoot(bosk).withString("distinctive string"),
+			driverFactory);
+		prepBosk.driver().flush();
+		((MongoDriver<TestEntity>)prepBosk.driver()).close();
+
+		TestEntity defaultState = initialRoot(prepBosk);
+		TestEntity mongoState = defaultState.withString("distinctive string");
+
+		mongoService.proxy().setConnectionCut(true);
+
+		Bosk<TestEntity> bosk = new Bosk<TestEntity>("Test bosk", TestEntity.class, this::initialRoot, driverFactory);
+		Refs refs = bosk.buildReferences(Refs.class);
+		BoskDriver<TestEntity> driver = bosk.driver();
+
+		try (var __ = bosk.readContext()) {
+			assertEquals(defaultState, bosk.rootReference().value(),
+				"Uses default state if database is unavailable");
+		}
+
+		assertThrows(MongoException.class, driver::flush,
+			"Flush disallowed during outage");
+		assertThrows(MongoException.class, () -> driver.submitReplacement(bosk.rootReference(), initialRoot(bosk)),
+			"Updates disallowed during outage");
+
+		mongoService.proxy().setConnectionCut(false);
+
+		driver.flush();
+		try (var __ = bosk.readContext()) {
+			assertEquals(mongoState, bosk.rootReference().value(),
+				"Updates to database state once it reconnects");
+		}
+
+		// Make a change to the bosk and verify that it gets through
+		driver.submitReplacement(refs.listingEntry(entity123), LISTING_ENTRY);
+		TestEntity expected = initialRoot(bosk)
+			.withListing(Listing.of(refs.catalog(), entity123));
+
+
+		driver.flush();
+		try (@SuppressWarnings("unused") Bosk<?>.ReadContext readContext = bosk.readContext()) {
+			assertEquals(expected, bosk.rootReference().value());
+		}
 	}
 
 	@ParametersByName

prdoyle/bosk

bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/MongoDriverSpecialTest.java
@@ -31,6 +31,7 @@ import java.util.Optional;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import lombok.Value;
+import lombok.var;
 import org.bson.BsonDocument;
 import org.bson.BsonNull;
 import org.bson.BsonString;
@@ -257,6 +258,57 @@ class MongoDriverSpecialTest implements TestParameters {
 			latecomerActual = latecomerBosk.rootReference().value();
 		}
 		assertEquals(expected, latecomerActual);
+	}
+
+	@ParametersByName
+	@DisruptsMongoService
+	void initialOutage_boskRecovers() throws InvalidTypeException, InterruptedException, IOException {
+		// Set up the database contents to be different from initialRoot
+		Bosk<TestEntity> prepBosk = new Bosk<TestEntity>(
+			"Prep bosk",
+			TestEntity.class,
+			bosk -> initialRoot(bosk).withString("distinctive string"),
+			driverFactory);
+		prepBosk.driver().flush();
+		((MongoDriver<TestEntity>)prepBosk.driver()).close();
+
+		TestEntity defaultState = initialRoot(prepBosk);
+		TestEntity mongoState = defaultState.withString("distinctive string");
+
+		mongoService.proxy().setConnectionCut(true);
+
+		Bosk<TestEntity> bosk = new Bosk<TestEntity>("Test bosk", TestEntity.class, this::initialRoot, driverFactory);
+		Refs refs = bosk.buildReferences(Refs.class);
+		BoskDriver<TestEntity> driver = bosk.driver();
+
+		try (var __ = bosk.readContext()) {
+			assertEquals(defaultState, bosk.rootReference().value(),
+				"Uses default state if database is unavailable");
+		}
+
+		assertThrows(FlushFailureException.class, driver::flush,
+			"Flush disallowed during outage");
+		assertThrows(FlushFailureException.class, () -> driver.submitReplacement(bosk.rootReference(), initialRoot(bosk)),
+			"Updates disallowed during outage");
+
+		mongoService.proxy().setConnectionCut(false);
+
+		driver.flush();
+		try (var __ = bosk.readContext()) {
+			assertEquals(mongoState, bosk.rootReference().value(),
+				"Updates to database state once it reconnects");
+		}
+
+		// Make a change to the bosk and verify that it gets through
+		driver.submitReplacement(refs.listingEntry(entity123), LISTING_ENTRY);
+		TestEntity expected = initialRoot(bosk)
+			.withListing(Listing.of(refs.catalog(), entity123));
+
+
+		driver.flush();
+		try (@SuppressWarnings("unused") Bosk<?>.ReadContext readContext = bosk.readContext()) {
+			assertEquals(expected, bosk.rootReference().value());
+		}
 	}
 
 	@ParametersByName