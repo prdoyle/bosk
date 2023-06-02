125ef28d2abe6f6db0b057f3ad6950e5b7e5a5f1
Patrick Doyle <pdoyle@venacorp.com>
2023-06-02 18:00:19 -0400

    FlushLock.close cause waiting threads to throw when awakened

--- bosk-mongo/src/main/java/io/vena/bosk/drivers/mongo/v2/FlushLock.java
+++ bosk-mongo/src/main/java/io/vena/bosk/drivers/mongo/v2/FlushLock.java
@@ -3,6 +3,7 @@ package io.vena.bosk.drivers.mongo.v2;
 import io.vena.bosk.drivers.mongo.MongoDriverSettings;
 import io.vena.bosk.exceptions.FlushFailureException;
 import io.vena.bosk.exceptions.NotYetImplementedException;
+import java.io.Closeable;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
@@ -17,11 +18,12 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 /**
  * Implements waiting mechanism for revision numbers
  */
-class FlushLock {
+class FlushLock implements Closeable {
 	private final MongoDriverSettings settings;
 	private final Lock queueLock = new ReentrantLock();
 	private final PriorityBlockingQueue<Waiter> queue = new PriorityBlockingQueue<>();
 	private volatile long alreadySeen;
+	private boolean isClosed;
 
 	/**
 	 * @param revisionAlreadySeen needs to be the exact revision from the database:
@@ -29,6 +31,7 @@ class FlushLock {
 	 * too new, and we'll proceed immediately without waiting for revisions that haven't happened yet.
 	 */
 	public FlushLock(MongoDriverSettings settings, long revisionAlreadySeen) {
+		LOGGER.debug("New flush lock from {}", revisionAlreadySeen);
 		this.settings = settings;
 		this.alreadySeen = revisionAlreadySeen;
 	}
@@ -58,7 +61,10 @@ class FlushLock {
 		if (revisionValue > past) {
 			LOGGER.debug("Awaiting revision {} > {}", revisionValue, past);
 			if (!semaphore.tryAcquire(settings.flushTimeoutMS(), MILLISECONDS)) {
-				throw new FlushFailureException("Timed out waiting for revision " + revisionValue);
+				throw new FlushFailureException("Timed out waiting for revision " + revisionValue + " > " + alreadySeen);
+			}
+			if (isClosed) {
+				throw new FlushFailureException("Wait aborted");
 			}
 		} else {
 			LOGGER.debug("Revision {} <= {} is in the past; don't wait", revisionValue, past);
@@ -95,6 +101,20 @@ class FlushLock {
 
 			alreadySeen = revisionValue;
 			LOGGER.debug("Finished {}", revisionValue);
+		} finally {
+			queueLock.unlock();
+		}
+	}
+
+	@Override
+	public void close() {
+		try {
+			queueLock.lock();
+			isClosed = true;
+			Waiter w;
+			while ((w = queue.poll()) != null) {
+				w.semaphore.release();
+			}
 		} finally {
 			queueLock.unlock();
 		}
