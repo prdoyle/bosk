8bd442b38622f255f90589ad9c1b71b6323bad16
Patrick Doyle <pdoyle@venacorp.com>
2023-06-02 10:53:08 -0400

    Moar tests

--- bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/MongoDriverResiliencyTest.java
+++ bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/MongoDriverResiliencyTest.java
@@ -3,16 +3,25 @@ package io.vena.bosk.drivers.mongo;
 import io.vena.bosk.Bosk;
 import io.vena.bosk.BoskDriver;
 import io.vena.bosk.Listing;
+import io.vena.bosk.drivers.mongo.Formatter.DocumentFields;
 import io.vena.bosk.drivers.state.TestEntity;
 import io.vena.bosk.exceptions.FlushFailureException;
 import io.vena.bosk.exceptions.InvalidTypeException;
 import io.vena.bosk.junit.ParametersByName;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 import lombok.var;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
+import org.bson.BsonNull;
+import org.junit.jupiter.api.Disabled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static io.vena.bosk.ListingEntry.LISTING_ENTRY;
 import static io.vena.bosk.drivers.mongo.MongoDriverSettings.ImplementationKind.RESILIENT;
+import static io.vena.bosk.drivers.mongo.v2.MainDriver.COLLECTION_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -20,7 +29,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * A set of tests that only work with {@link io.vena.bosk.drivers.mongo.MongoDriverSettings.ImplementationKind#RESILIENT}
  */
 public class MongoDriverResiliencyTest extends AbstractMongoDriverTest {
-
 	@ParametersByName
 	public MongoDriverResiliencyTest(MongoDriverSettings.MongoDriverSettingsBuilder driverSettings) {
 		super(driverSettings);
@@ -30,31 +38,26 @@ public class MongoDriverResiliencyTest extends AbstractMongoDriverTest {
 	static Stream<MongoDriverSettings.MongoDriverSettingsBuilder> driverSettings() {
 		return Stream.of(
 			MongoDriverSettings.builder()
-				.database("boskTestDB_resiliency")
+				.database("boskResiliencyTestDB_" + dbCounter.incrementAndGet())
 				.implementationKind(RESILIENT)
+				.testing(MongoDriverSettings.Testing.builder()
+					.eventDelayMS(100)
+					.build())
 		);
 	}
 
 	@ParametersByName
 	@DisruptsMongoService
-	void initialOutage_boskRecovers() throws InvalidTypeException, InterruptedException, IOException {
+	void initialOutage_recovers() throws InvalidTypeException, InterruptedException, IOException {
 		// Set up the database contents to be different from initialRoot
-		Bosk<TestEntity> prepBosk = new Bosk<TestEntity>(
-			"Prep bosk",
-			TestEntity.class,
-			bosk -> initialRoot(bosk).withString("distinctive string"),
-			driverFactory);
-		prepBosk.driver().flush();
-		((MongoDriver<TestEntity>)prepBosk.driver()).close();
-
-		TestEntity defaultState = initialRoot(prepBosk);
-		TestEntity mongoState = defaultState.withString("distinctive string");
+		TestEntity initialState = initializeDatabase("distinctive string", false);
 
 		mongoService.proxy().setConnectionCut(true);
 
 		Bosk<TestEntity> bosk = new Bosk<TestEntity>("Test bosk", TestEntity.class, this::initialRoot, driverFactory);
 		MongoDriverSpecialTest.Refs refs = bosk.buildReferences(MongoDriverSpecialTest.Refs.class);
 		BoskDriver<TestEntity> driver = bosk.driver();
+		TestEntity defaultState = initialRoot(bosk);
 
 		try (var __ = bosk.readContext()) {
 			assertEquals(defaultState, bosk.rootReference().value(),
@@ -70,7 +73,7 @@ public class MongoDriverResiliencyTest extends AbstractMongoDriverTest {
 
 		driver.flush();
 		try (var __ = bosk.readContext()) {
-			assertEquals(mongoState, bosk.rootReference().value(),
+			assertEquals(initialState, bosk.rootReference().value(),
 				"Updates to database state once it reconnects");
 		}
 
@@ -87,4 +90,116 @@ public class MongoDriverResiliencyTest extends AbstractMongoDriverTest {
 		}
 	}
 
+	@ParametersByName
+	@UsesMongoService
+	void databaseDeleted_recovers() throws InvalidTypeException, InterruptedException, IOException {
+		testRecoveryAfterDeletion(false, () -> {
+			LOGGER.debug("Drop database");
+			mongoService.client()
+				.getDatabase(driverSettings.database())
+				.drop();
+		});
+	}
+
+	@ParametersByName
+	@UsesMongoService
+	void collectionDeleted_recovers() throws InvalidTypeException, InterruptedException, IOException {
+		testRecoveryAfterDeletion(false, () -> {
+			LOGGER.debug("Drop collection");
+			mongoService.client()
+				.getDatabase(driverSettings.database())
+				.getCollection(COLLECTION_NAME)
+				.drop();
+		});
+	}
+
+	@ParametersByName
+	@UsesMongoService
+	@Disabled("Document deletion currently does not initiate recovery, because deletion also happens in refurbish; but this means it also can't cope with the revision field decreasing")
+	void documentDeleted_recovers() throws InvalidTypeException, InterruptedException, IOException {
+		testRecoveryAfterDeletion(false, () -> {
+			LOGGER.debug("Delete document");
+			mongoService.client()
+				.getDatabase(driverSettings.database())
+				.getCollection(COLLECTION_NAME)
+				.deleteMany(new BsonDocument());
+		});
+	}
+
+	@ParametersByName
+	@UsesMongoService
+	void revisionDeleted_recovers() throws InvalidTypeException, InterruptedException, IOException {
+		testRecoveryAfterDeletion(true, () -> {
+			LOGGER.debug("Delete revision");
+			mongoService.client()
+				.getDatabase(driverSettings.database())
+				.getCollection(COLLECTION_NAME)
+				.updateOne(
+					new BsonDocument(),
+					new BsonDocument("$unset", new BsonDocument(DocumentFields.revision.name(), new BsonNull())) // Value is ignored
+				);
+		});
+	}
+
+	@ParametersByName
+	@UsesMongoService
+	@Disabled("Situations where the revision number goes backward currently don't work")
+	void revisionDecreased_recovers() throws InvalidTypeException, InterruptedException, IOException {
+		LOGGER.debug("Decrease revision");
+		mongoService.client()
+			.getDatabase(driverSettings.database())
+			.getCollection(COLLECTION_NAME)
+			.updateOne(
+				new BsonDocument(),
+				new BsonDocument("$dec", new BsonDocument(DocumentFields.revision.name(), new BsonInt64(1)))
+			);
+	}
+
+	private TestEntity initializeDatabase(String distinctiveString, boolean doRefurbish) throws IOException, InterruptedException, InvalidTypeException {
+		Bosk<TestEntity> prepBosk = new Bosk<TestEntity>(
+			"Prep bosk " + prepBoskCounter.incrementAndGet(),
+			TestEntity.class,
+			bosk -> initialRoot(bosk).withString(distinctiveString),
+			driverFactory);
+		MongoDriver<TestEntity> driver = (MongoDriver<TestEntity>) prepBosk.driver();
+		if (doRefurbish) {
+			driver.refurbish();
+		}
+		driver.flush();
+		driver.close();
+
+		return initialRoot(prepBosk).withString(distinctiveString);
+	}
+
+	private void testRecoveryAfterDeletion(boolean doRefurbish, Runnable action) throws IOException, InterruptedException, InvalidTypeException {
+		LOGGER.debug("Setup database to beforeState");
+		TestEntity beforeState = initializeDatabase("before deletion", false);
+
+		Bosk<TestEntity> bosk = new Bosk<TestEntity>("Test bosk", TestEntity.class, this::initialRoot, driverFactory);
+		try (var __ = bosk.readContext()) {
+			assertEquals(beforeState, bosk.rootReference().value());
+		}
+
+		action.run();
+
+		LOGGER.debug("Ensure flush throws");
+		assertThrows(FlushFailureException.class, () -> bosk.driver().flush());
+		try (var __ = bosk.readContext()) {
+			assertEquals(beforeState, bosk.rootReference().value());
+		}
+
+		LOGGER.debug("Setup database to afterState");
+		TestEntity afterState = initializeDatabase("after deletion", doRefurbish);
+
+		LOGGER.debug("Ensure flush works");
+		bosk.driver().flush();
+		try (var __ = bosk.readContext()) {
+			assertEquals(afterState, bosk.rootReference().value());
+		}
+	}
+
+	private static final AtomicInteger dbCounter = new AtomicInteger(0);
+	private static final AtomicInteger prepBoskCounter = new AtomicInteger(0);
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MongoDriverResiliencyTest.class);
 }
