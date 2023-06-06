ade10038c2f3098dbb14586205cd43809134ce29
Patrick Doyle <pdoyle@venacorp.com>
2023-06-05 21:31:50 -0400

    I'm bringing STABLE back

--- bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/TestParameters.java
+++ bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/TestParameters.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 import static io.vena.bosk.drivers.mongo.MongoDriverSettings.ImplementationKind.RESILIENT;
+import static io.vena.bosk.drivers.mongo.MongoDriverSettings.ImplementationKind.STABLE;
 
 public interface TestParameters {
 	AtomicInteger dbCounter = new AtomicInteger(0);
@@ -13,9 +14,9 @@ public interface TestParameters {
 	static Stream<MongoDriverSettingsBuilder> driverSettings() {
 		String prefix = "boskTestDB_" + dbCounter.incrementAndGet();
 		return Stream.of(
-//			MongoDriverSettings.builder()
-//				.database(prefix + "_stable")
-//				.implementationKind(STABLE),
+			MongoDriverSettings.builder()
+				.database(prefix + "_stable")
+				.implementationKind(STABLE),
 			MongoDriverSettings.builder()
 				.database(prefix + "_resilient")
 				.implementationKind(RESILIENT)
