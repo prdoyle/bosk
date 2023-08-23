2b1023dfa07c992036f2c43623a478f85b353680
Patrick Doyle <pdoyle@venacorp.com>
2023-08-23 08:52:26 -0400

    Change WebProperties to a record

--- bosk-spring-boot-3/src/main/java/io/vena/bosk/spring/boot/WebProperties.java
+++ bosk-spring-boot-3/src/main/java/io/vena/bosk/spring/boot/WebProperties.java
@@ -1,13 +1,9 @@
 package io.vena.bosk.spring.boot;
 
-import lombok.Value;
-import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "bosk.web")
-@Value
-@Accessors(fluent = false)
-public class WebProperties {
-	Boolean readContext;
-	String servicePath;
-}
+public record WebProperties(
+	Boolean readContext,
+	String servicePath
+) {}
