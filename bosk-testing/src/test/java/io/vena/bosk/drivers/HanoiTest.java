c2db41ea8146a75a84bc210195c929738b40e004
Patrick Doyle <pdoyle@venacorp.com>
2023-10-07 12:21:44 -0400

    HanoiTest

---
+++ bosk-testing/src/test/java/io/vena/bosk/drivers/HanoiTest.java
@@ -0,0 +1,263 @@
+package io.vena.bosk.drivers;
+
+import io.vena.bosk.BindingEnvironment;
+import io.vena.bosk.Bosk;
+import io.vena.bosk.Catalog;
+import io.vena.bosk.CatalogReference;
+import io.vena.bosk.Entity;
+import io.vena.bosk.Identifier;
+import io.vena.bosk.Listing;
+import io.vena.bosk.ListingEntry;
+import io.vena.bosk.Reference;
+import io.vena.bosk.StateTreeNode;
+import io.vena.bosk.annotations.ReferencePath;
+import io.vena.bosk.exceptions.InvalidTypeException;
+import java.util.concurrent.Semaphore;
+import java.util.stream.IntStream;
+import lombok.Value;
+import lombok.var;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.vena.bosk.ListingEntry.LISTING_ENTRY;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HanoiTest {
+	Bosk<HanoiState> bosk;
+	Refs refs;
+	Semaphore quiescenceSemaphore;
+	volatile boolean isQuiescent;
+
+	public interface Refs {
+		@ReferencePath("/puzzles") CatalogReference<Puzzle> puzzles();
+		@ReferencePath("/puzzles/-puzzle-") Reference<Puzzle> puzzle(Identifier puzzle);
+		@ReferencePath("/solved/-puzzle-") Reference<ListingEntry> solved(Identifier puzzle);
+		@ReferencePath("/puzzles/-puzzle-/startingTower") Reference<Reference<Tower>> startingTower(Identifier puzzle);
+		@ReferencePath("/puzzles/-puzzle-/towers/-tower-") Reference<Tower> tower(Identifier puzzle, Identifier tower);
+		@ReferencePath("/puzzles/-puzzle-/towers/-tower-/discs/-disc-") Reference<Disc> anyDisc();
+		@ReferencePath("/puzzles/-puzzle-/towers/-tower-/discs/-disc-") Reference<Disc> disc(Identifier puzzle, Identifier tower, Identifier disc);
+	}
+
+	@BeforeEach
+	void setup() throws InvalidTypeException {
+		isQuiescent = true;
+		bosk = new Bosk<HanoiState>(
+			"Hanoi",
+			HanoiState.class,
+			this::defaultRoot,
+			Bosk::simpleDriver
+		);
+		refs = bosk.rootReference().buildReferences(Refs.class);
+		quiescenceSemaphore = new Semaphore(0);
+		bosk.registerHook("quiescence", bosk.rootReference(), this::quiescenceHook);
+		bosk.registerHook("discMoved", refs.anyDisc(), this::discMoved);
+	}
+
+	@Test
+	void onePuzzle() throws InterruptedException {
+		int numDiscs = 6;
+		isQuiescent = false;
+		bosk.driver().submitReplacement(refs.puzzle(PUZZLE_1),
+			newPuzzle(PUZZLE_1, numDiscs));
+		quiescenceSemaphore.acquire();
+		try (var __ = bosk.readContext()) {
+			assertEquals(new HanoiState(
+				Catalog.of(solvedPuzzle(PUZZLE_1, numDiscs)),
+				Listing.of(refs.puzzles(), PUZZLE_1)
+			), bosk.rootReference().valueIfExists());
+		}
+		assertTrue(isQuiescent);
+	}
+
+	private void quiescenceHook(Reference<HanoiState> ref) {
+		if (isQuiescent) {
+			LOGGER.debug("Already quiescent");
+		} else {
+			HanoiState state = ref.value();
+			if (state.puzzles.size() == state.solved.size()) {
+				LOGGER.debug("Became quiescent");
+				isQuiescent = true;
+				quiescenceSemaphore.release();
+			} else {
+				LOGGER.trace("Still solving");
+			}
+		}
+	}
+
+	private void discMoved(Reference<Disc> ref) {
+		Disc moved = ref.valueIfExists();
+		if (moved == null) {
+			LOGGER.debug("Ignoring disc deletion: {}", ref);
+			return;
+		}
+		if (LOGGER.isTraceEnabled()) {
+			LOGGER.trace("discMoved state:\n\t{}", bosk.rootReference().value());
+		}
+		BindingEnvironment env = refs.anyDisc().parametersFrom(ref.path());
+		Identifier puzzle = env.get("puzzle");
+		Identifier landingTowerID = env.get("tower");
+		Reference<Tower> landingTower = refs.tower(puzzle, landingTowerID);
+		Reference<Tower> t1 = refs.tower(puzzle, nextTower(landingTowerID));
+		Tower tower1 = t1.value();
+		Reference<Tower> t2 = refs.tower(puzzle, nextTower(tower1.id()));
+		Tower tower2 = t2.value();
+		LOGGER.trace("-- Landing={} t1={} t2={}", landingTowerID, tower1.id, tower2.id);
+
+		// Rules:
+		// 1. Never move the same disc twice
+		// 2. If possible, move a disc "clockwise" to the nextTower
+
+		if (tower1.discs().isEmpty()) {
+			if (tower2.discs().isEmpty()) {
+				if (refs.startingTower(puzzle).value().equals(landingTower)) {
+					LOGGER.debug("First move from {} to empty {}", landingTowerID, t1);
+					submitMove(landingTower.value(), tower1, landingTower.value().topDisc(), puzzle);
+				} else {
+					LOGGER.debug("Puzzle is solved");
+					bosk.driver().submitReplacement(refs.solved(puzzle), LISTING_ENTRY);
+				}
+			} else {
+				var disc2 = tower2.topDisc();
+				if (disc2.size() < moved.size()) {
+					LOGGER.debug("Move from t2 {} to landing tower", t2);
+					submitMove(tower2, landingTower.value(), tower2.topDisc(), puzzle);
+				} else {
+					LOGGER.debug("Move from t2 {} to empty t1", t2);
+					submitMove(tower2, tower1, tower2.topDisc(), puzzle);
+				}
+			}
+		} else if (tower2.discs().isEmpty()) {
+			LOGGER.debug("Move from non-empty t1 {}", t1);
+			submitMove(tower1, tower2, tower1.topDisc(), puzzle);
+		} else {
+			var disc1 = tower1.topDisc();
+			var disc2 = tower2.topDisc();
+			if (disc1.size() < disc2.size()) {
+				LOGGER.debug("Move smaller {} onto {}", t1, t2);
+				submitMove(tower1, tower2, disc1, puzzle);
+			} else if (disc2.size() < moved.size()) {
+				LOGGER.debug("Move {} onto landing tower", t2);
+				submitMove(tower2, landingTower.value(), disc2, puzzle);
+			} else {
+				LOGGER.debug("Move {} onto bigger {}", t2, t1);
+				submitMove(tower2, tower1, disc2, puzzle);
+			}
+		}
+	}
+
+	private void submitMove(Tower from, Tower to, Disc disc, Identifier puzzle) {
+		LOGGER.debug("-> Moving {}", disc.id());
+		bosk.driver().submitDeletion(refs.disc(puzzle, from.id(), disc.id()));
+		bosk.driver().submitReplacement(refs.disc(puzzle, to.id(), disc.id()), disc);
+	}
+
+	private Puzzle newPuzzle(Identifier id, int numDiscs) {
+		return new Puzzle(
+			id,
+			Catalog.of(
+				newTower(LEFT, numDiscs),
+				newTower(MIDDLE, 0),
+				newTower(RIGHT, 0)
+			),
+			refs.tower(id, LEFT)
+		);
+	}
+
+	private Puzzle solvedPuzzle(Identifier id, int numDiscs) {
+		if (numDiscs % 2 == 1) {
+			// Odd towers end up on the middle
+			return new Puzzle(
+				id,
+				Catalog.of(
+					newTower(LEFT, 0),
+					newTower(MIDDLE, numDiscs),
+					newTower(RIGHT, 0)
+				),
+				refs.tower(id, LEFT)
+			);
+		} else {
+			// Even towers end up on the right
+			return new Puzzle(
+				id,
+				Catalog.of(
+					newTower(LEFT, 0),
+					newTower(MIDDLE, 0),
+					newTower(RIGHT, numDiscs)
+				),
+				refs.tower(id, LEFT)
+			);
+		}
+	}
+
+	private Tower newTower(Identifier id, int numDiscs) {
+		return new Tower(id, Catalog.of(
+			IntStream.range(0, numDiscs)
+				.map(i -> numDiscs - i)
+				.mapToObj(Integer::toString)
+				.map(Identifier::from)
+				.map(Disc::new)
+		));
+	}
+
+	private HanoiState defaultRoot(Bosk<HanoiState> bosk) throws InvalidTypeException {
+		CatalogReference<Puzzle> puzzlesRef = bosk.rootReference().thenCatalog(Puzzle.class, "puzzles");
+		return new HanoiState(
+			Catalog.empty(),
+			Listing.empty(puzzlesRef)
+		);
+	}
+
+	@Value
+	public static class HanoiState implements StateTreeNode {
+		Catalog<Puzzle> puzzles;
+		Listing<Puzzle> solved;
+	}
+
+	@Value
+	public static class Puzzle implements Entity {
+		Identifier id;
+		Catalog<Tower> towers;
+		Reference<Tower> startingTower;
+	}
+
+	@Value
+	public static class Tower implements Entity {
+		Identifier id;
+		Catalog<Disc> discs;
+
+		public Disc topDisc() {
+			return discs.stream().reduce(null, (a,b) -> b);
+		}
+
+		@Override public String toString() { return discs.asCollection().toString(); }
+	}
+
+	@Value
+	public static class Disc implements Entity {
+		Identifier id;
+
+		public int size() { return Integer.parseInt(id.toString()); }
+
+		@Override public String toString() { return id.toString(); }
+	}
+
+	private Identifier nextTower(Identifier t) {
+		if (LEFT.equals(t)) {
+			return MIDDLE;
+		} else if (MIDDLE.equals(t)) {
+			return RIGHT;
+		} else {
+			return LEFT;
+		}
+	}
+
+	private static final Identifier PUZZLE_1 = Identifier.from("p1");
+	private static final Identifier LEFT    = Identifier.from("left");
+	private static final Identifier MIDDLE  = Identifier.from("middle");
+	private static final Identifier RIGHT   = Identifier.from("right");
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HanoiTest.class);
+}
