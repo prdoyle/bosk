072aae241fdf234ffe3dc1aa13dda16a060c1dd5
Patrick Doyle <p.r.doyle@gmail.com>
2024-06-15 09:36:30 -0400

    getDeclaredMethodsInOrder.
    Java's Class.getDeclaredMethods offers no guarantees about order.
    Since we're using ASM anyway, might as well use that to determine
    bytecode order.
    
    Without this, it's hard to write tests that produce stable results.

---
+++ bosk-core/src/test/java/works/bosk/util/ReflectionHelpersTest.java
@@ -0,0 +1,43 @@
+package works.bosk.util;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ReflectionHelpersTest {
+
+	@Test
+	void getDeclaredMethodsInOrder_correctOrder() throws NoSuchMethodException {
+		List<Method> actual = ReflectionHelpers.getDeclaredMethodsInOrder(ClassWithSomeMethods.class);
+		List<Method> expected = List.of(
+			ClassWithSomeMethods.class.getDeclaredMethod("first"),
+			ClassWithSomeMethods.class.getDeclaredMethod("secondOverloaded", int.class),
+			ClassWithSomeMethods.class.getDeclaredMethod("secondOverloaded", long.class),
+			ClassWithSomeMethods.class.getDeclaredMethod("secondOverloaded", float.class),
+			ClassWithSomeMethods.class.getDeclaredMethod("secondOverloaded", String.class),
+			ClassWithSomeMethods.class.getDeclaredMethod("third")
+		);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void getDeclaredMethodsInOrder_worksWithBuiltInClasses() throws NoSuchMethodException {
+		List<Method> actual = ReflectionHelpers.getDeclaredMethodsInOrder(Runnable.class);
+		List<Method> expected = List.of(
+			Runnable.class.getDeclaredMethod("run")
+		);
+		assertEquals(expected, actual);
+	}
+
+	public interface ClassWithSomeMethods {
+		void first();
+		void secondOverloaded(int x);
+		void secondOverloaded(long x);
+		void secondOverloaded(float x);
+		void secondOverloaded(String s);
+		void third();
+	}
+
+}
