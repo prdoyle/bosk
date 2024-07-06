976b905898e23fac589b681da62aa489d873d0e7
Patrick Doyle <p.r.doyle@gmail.com>
2024-07-06 15:43:14 -0400

    Test Fix: Shut down MongoDriver after MongoDriverHanoiTest.
    Prevents lots of unnecessary exceptions and logging. (The tests were not
    actually failing though.)

--- bosk-mongo/src/test/java/works/bosk/drivers/mongo/MongoDriverHanoiTest.java
+++ bosk-mongo/src/test/java/works/bosk/drivers/mongo/MongoDriverHanoiTest.java
@@ -1,21 +1,28 @@
 package works.bosk.drivers.mongo;
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeAll;
+import works.bosk.DriverStack;
 import works.bosk.drivers.HanoiTest;
 import works.bosk.junit.ParametersByName;
 
 @UsesMongoService
 public class MongoDriverHanoiTest extends HanoiTest {
 	private static MongoService mongoService;
+	private Queue<Runnable> shutdownOperations = new ConcurrentLinkedDeque<>();
 
 	@ParametersByName
 	public MongoDriverHanoiTest(TestParameters.ParameterSet parameters) {
 		MongoDriverSettings settings = parameters.driverSettingsBuilder().build();
-		this.driverFactory = MongoDriver.factory(
-			mongoService.clientSettings(),
-			settings,
-			new BsonPlugin()
+		this.driverFactory = DriverStack.of(
+			(b,d) -> { shutdownOperations.add(((MongoDriver<?>)d)::close); return d;},
+			MongoDriver.factory(
+				mongoService.clientSettings(),
+				settings,
+				new BsonPlugin()
+			)
 		);
 		mongoService.client()
 			.getDatabase(settings.database())
