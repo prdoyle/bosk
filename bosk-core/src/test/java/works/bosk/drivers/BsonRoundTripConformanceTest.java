108f0074b02f1da2cd783a830ad828d21c6eafc2
Patrick Doyle <p.r.doyle@gmail.com>
2024-07-20 11:35:44 -0400

    JacksonRoundTripConformanceTest

--- bosk-core/src/test/java/works/bosk/drivers/BsonRoundTripConformanceTest.java
+++ bosk-core/src/test/java/works/bosk/drivers/BsonRoundTripConformanceTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 
 import static works.bosk.AbstractRoundTripTest.bsonRoundTripFactory;
 
+/**
+ * TODO: Move to bosk-mongo
+ */
 public class BsonRoundTripConformanceTest extends DriverConformanceTest {
 	@BeforeEach
 	void setupDriverFactory() {
