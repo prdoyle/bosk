d0196f8585dc9071e221ca2118a9ec5a4bf2d2da
Patrick Doyle <p.r.doyle@gmail.com>
2024-08-14 21:17:39 -0400

    First basic conformance test passes

--- bosk-postgres/src/test/java/works/bosk/drivers/postgresql/PostgresDriverConformanceTest.java
+++ bosk-postgres/src/test/java/works/bosk/drivers/postgresql/PostgresDriverConformanceTest.java
@@ -1,13 +1,16 @@
 package works.bosk.drivers.postgresql;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,20 +24,44 @@ import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
 
 class PostgresDriverConformanceTest extends DriverConformanceTest {
 	static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	public static final String DATABASE = "bosk_test_database";
 	private final Deque<Runnable> tearDownActions = new ArrayDeque<>();
 
-	final AtomicInteger dbCounter = new AtomicInteger(0);
+	final AtomicInteger schemaCounter = new AtomicInteger(0);
+
+	@BeforeAll
+	static void setupDatabase() {
+		try (
+			var connection = getConnection();
+			var stmt = connection.createStatement()
+		) {
+			stmt.execute("CREATE DATABASE " + quoted(DATABASE));
+		} catch (SQLException e) {
+			throw new AssertionError("Unexpected error setting up database", e);
+		}
+	}
+
+	@AfterAll
+	static void cleanupDatabase() {
+		try (
+			var connection = getConnection();
+			var stmt = connection.createStatement()
+		) {
+			stmt.execute("DROP DATABASE IF EXISTS " + quoted(DATABASE));
+		} catch (SQLException e) {
+			throw new AssertionError("Unexpected error cleaning up database", e);
+		}
+	}
 
 	@BeforeEach
 	void setupDriverFactory() {
 		driverFactory = (boskInfo, downstream) -> {
 			PostgresDriverSettings settings = new PostgresDriverSettings(
 				"jdbc:tc:postgresql://localhost:5432",
-				"bosk_test_database_" + dbCounter.incrementAndGet(),
-				"bosk_test_schema"
+				DATABASE,
+				"bosk_test_schema_" + schemaCounter.incrementAndGet()
 			);
-			setupDatabase(settings);
-			tearDownActions.addFirst(() -> cleanupDatabase(settings));
+			tearDownActions.addFirst(() -> cleanupSchema(settings));
 			return PostgresDriver.<TestEntity>factory(
 				settings,
 				b -> new ObjectMapper()
@@ -49,26 +76,19 @@ class PostgresDriverConformanceTest extends DriverConformanceTest {
 		tearDownActions.forEach(Runnable::run);
 	}
 
-	private static void setupDatabase(PostgresDriverSettings settings) {
+	private void cleanupSchema(PostgresDriverSettings settings) {
 		try (
-			var connection = DriverManager.getConnection(settings.url(), new Properties());
+			var connection = getConnection();
 			var stmt = connection.createStatement()
 		) {
-			stmt.execute("CREATE DATABASE " + quoted(settings.database()));
+			stmt.execute("DROP SCHEMA IF EXISTS " + quoted(settings.schema()));
 		} catch (SQLException e) {
-			throw new AssertionError("Unexpected error setting up database", e);
+			throw new AssertionError("Unexpected error cleaning up schema", e);
 		}
 	}
 
-	private static void cleanupDatabase(PostgresDriverSettings settings) {
-		try (
-			var connection = DriverManager.getConnection(settings.url(), new Properties());
-			var stmt = connection.createStatement()
-		) {
-			stmt.execute("DROP DATABASE IF EXISTS " + quoted(settings.database()));
-		} catch (SQLException e) {
-			throw new AssertionError("Unexpected error cleaning up database", e);
-		}
+	private static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection("jdbc:tc:postgresql://localhost:5432", new Properties());
 	}
 
 	private static String quoted(String raw) {
