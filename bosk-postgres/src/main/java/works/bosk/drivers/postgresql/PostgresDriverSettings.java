c06134ce2b152b9e18ea161884dee708cb8cc6ab
Patrick Doyle <p.r.doyle@gmail.com>
2024-08-16 09:07:46 -0400

    More tests working

--- bosk-postgres/src/main/java/works/bosk/drivers/postgresql/PostgresDriverSettings.java
+++ bosk-postgres/src/main/java/works/bosk/drivers/postgresql/PostgresDriverSettings.java
@@ -2,6 +2,12 @@ package works.bosk.drivers.postgresql;
 
 public record PostgresDriverSettings(
 	String url, // Without the database
-	String database,
-	String schema
-) { }
+	String database
+) {
+	@Override
+	public String toString() {
+		return "PostgresDriverSettings[" +
+			"url=" + url + ", " +
+			"database=" + database + ']';
+	}
+}
