d922b6c8e4ce6a10f40cf43ae1ed40957803f2fe
Patrick Doyle <p.r.doyle@gmail.com>
2024-08-19 09:39:52 -0400

    Fix ReplicaSet.initialRoot.
    Identify (fairly arbitrarily) one of the replicas as the "primary" and
    use its state as the initialRoot for other bosks. The primary will be
    the bosk whose driver is constructed first using BroadcastDriver.
    This will usually correspond to the bosk whose state gets updated first,
    making it a good choice to supply the initial root.

--- bosk-core/src/test/java/works/bosk/drivers/ReplicaSetTest.java
+++ bosk-core/src/test/java/works/bosk/drivers/ReplicaSetTest.java
@@ -18,11 +18,28 @@ public class ReplicaSetTest extends AbstractDriverTest {
 	@Test
 	void joinAfterUpdate_correctInitialState() throws InvalidTypeException {
 		var replicaSet = new ReplicaSet<TestEntity>();
-		var bosk1 = new Bosk<TestEntity>(boskName("bosk1"), TestEntity.class, AbstractDriverTest::initialRoot, replicaSet.driverFactory());
+		var bosk1 = new Bosk<>(boskName("bosk1"), TestEntity.class, AbstractDriverTest::initialRoot, replicaSet.driverFactory());
 		var refs1 = bosk1.rootReference().buildReferences(Refs.class);
 		bosk1.driver().submitReplacement(refs1.string(), "New value");
 
-		var bosk2 = new Bosk<TestEntity>(boskName("bosk2"), TestEntity.class, AbstractDriverTest::initialRoot, replicaSet.driverFactory());
+		var bosk2 = new Bosk<>(boskName("bosk2"), TestEntity.class, AbstractDriverTest::initialRoot, replicaSet.driverFactory());
+		var refs2 = bosk2.rootReference().buildReferences(Refs.class);
+		try (var _ = bosk2.readContext()) {
+			assertEquals("New value", refs2.string().value());
+		}
+	}
+
+	@Test
+	void secondaryConstructedInPrimaryReadContext_seesLatestState() throws InvalidTypeException {
+		var replicaSet = new ReplicaSet<TestEntity>();
+		var bosk1 = new Bosk<>(boskName("bosk1"), TestEntity.class, AbstractDriverTest::initialRoot, replicaSet.driverFactory());
+		var refs1 = bosk1.rootReference().buildReferences(Refs.class);
+
+		Bosk<TestEntity> bosk2;
+		try (var _ = bosk1.readContext()) {
+			bosk1.driver().submitReplacement(refs1.string(), "New value");
+			bosk2 = new Bosk<>(boskName("bosk2"), TestEntity.class, AbstractDriverTest::initialRoot, replicaSet.driverFactory());
+		}
 		var refs2 = bosk2.rootReference().buildReferences(Refs.class);
 		try (var _ = bosk2.readContext()) {
 			assertEquals("New value", refs2.string().value());
