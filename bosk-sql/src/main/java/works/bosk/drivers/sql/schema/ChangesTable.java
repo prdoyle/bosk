8d6a8b7824a5e007cd517570a58851ee3977c4e1
Patrick Doyle <p.r.doyle@gmail.com>
2025-03-01 18:24:52 -0500

    Cosmetic changes

--- bosk-sql/src/main/java/works/bosk/drivers/sql/schema/ChangesTable.java
+++ bosk-sql/src/main/java/works/bosk/drivers/sql/schema/ChangesTable.java
@@ -2,10 +2,8 @@ package works.bosk.drivers.sql.schema;
 
 import org.jooq.Record;
 import org.jooq.TableField;
-import org.jooq.impl.DSL;
 import org.jooq.impl.TableImpl;
 
-import static org.jooq.impl.DSL.field;
 import static org.jooq.impl.DSL.name;
 import static org.jooq.impl.SQLDataType.BIGINT;
 import static org.jooq.impl.SQLDataType.CLOB;
@@ -18,7 +16,7 @@ public class ChangesTable extends TableImpl<org.jooq.Record> {
 		name("ref"), VARCHAR.notNull());
 	public final TableField<Record, String> NEW_STATE = createField(
 		name("new_state"), CLOB.null_());
-	public final TableField<Record, String> DIAGONSTICS = createField(
+	public final TableField<Record, String> DIAGNOSTICS = createField(
 		name("diagnostics"), CLOB.notNull());
 
 	ChangesTable() {
