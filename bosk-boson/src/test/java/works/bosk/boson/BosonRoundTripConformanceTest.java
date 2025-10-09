6f5afd2d543cfecd6f6f206c68fa47e2e6c1f19d
Patrick Doyle <p.r.doyle@gmail.com>
2025-10-08 22:54:26 -0400

    Refactor: BosonRoundTripDriver

--- bosk-boson/src/test/java/works/bosk/boson/BosonRoundTripConformanceTest.java
+++ bosk-boson/src/test/java/works/bosk/boson/BosonRoundTripConformanceTest.java
@@ -5,6 +5,10 @@ import java.io.StringWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import works.bosk.AbstractRoundTripTest;
+import works.bosk.BoskDriver;
+import works.bosk.BoskInfo;
+import works.bosk.DriverFactory;
+import works.bosk.Entity;
 import works.bosk.Reference;
 import works.bosk.json.codec.CharArrayReader;
 import works.bosk.json.codec.Codec;
@@ -19,32 +23,43 @@ import works.bosk.testing.drivers.DriverConformanceTest;
 
 class BosonRoundTripConformanceTest extends DriverConformanceTest {
 	BosonRoundTripConformanceTest() {
-		driverFactory = (b,d) -> {
+		driverFactory = BosonRoundTripDriver.factory();
+	}
+
+	public static class BosonRoundTripDriver extends AbstractRoundTripTest.PreprocessingDriver {
+		private final TypeMap typeMap;
+		private final Codec codec;
+
+		private BosonRoundTripDriver(BoskInfo<?> b, BoskDriver d) {
+			super(d);
 			var rootType = DataType.of(b.rootReference().targetType());
 			TypeScanner.Bundle bundle = new BosonSerializer().bundleFor(b);
 			LOGGER.debug("Creating the real TypeScanner now for root type {}", rootType);
-			TypeMap typeMap = new TypeScanner(TypeMap.Settings.DEFAULT.withCompiled(false))
+			this.typeMap = new TypeScanner(TypeMap.Settings.DEFAULT.withCompiled(false))
 				.addLast(bundle)
 				.scan(rootType)
 				.build();
-			Codec codec = CodecBuilder.of(typeMap).build();
-			return new AbstractRoundTripTest.PreprocessingDriver(d) {
-				@Override
-				protected <T> T preprocess(Reference<T> reference, T newValue) {
-					JsonValueSpec targetSpec = typeMap.get(DataType.of(reference.targetType()));
-					Generator generator = codec.generatorFor(targetSpec);
-					var writer = new StringWriter();
-					generator.generate(writer, newValue);
-					Parser parser = codec.parserFor(targetSpec);
-					try {
-						Object parsed = parser.parse(new CharArrayReader(writer.toString()));
-						return reference.targetClass().cast(parsed);
-					} catch (IOException e) {
-						throw new AssertionError("Unexpected exception", e);
-					}
-				}
-			};
-		};
+			this.codec = CodecBuilder.of(typeMap).build();
+		}
+
+		public static <R extends Entity> DriverFactory<R> factory() {
+			return BosonRoundTripDriver::new;
+		}
+
+		@Override
+		protected <T> T preprocess(Reference<T> reference, T newValue) {
+			JsonValueSpec targetSpec = typeMap.get(DataType.of(reference.targetType()));
+			Generator generator = codec.generatorFor(targetSpec);
+			var writer = new StringWriter();
+			generator.generate(writer, newValue);
+			Parser parser = codec.parserFor(targetSpec);
+			try {
+				Object parsed = parser.parse(new CharArrayReader(writer.toString()));
+				return reference.targetClass().cast(parsed);
+			} catch (IOException e) {
+				throw new AssertionError("Unexpected exception", e);
+			}
+		}
 	}
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(BosonRoundTripConformanceTest.class);
