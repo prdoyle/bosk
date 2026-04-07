82af700570bf763b75f44cabaa6fe8d2646caba6
Patrick Doyle <p.r.doyle@gmail.com>
2026-04-07 18:17:14 -0400

    Actually test JsonNodeDriver

--- bosk-jackson/src/test/java/works/bosk/jackson/JsonNodeDriverConformanceTest.java
+++ bosk-jackson/src/test/java/works/bosk/jackson/JsonNodeDriverConformanceTest.java
@@ -1,15 +1,37 @@
 package works.bosk.jackson;
 
 import org.junit.jupiter.api.BeforeEach;
+import tools.jackson.databind.JsonNode;
+import works.bosk.BoskDriver;
 import works.bosk.junit.InjectFrom;
 import works.bosk.testing.drivers.AbstractDriverTest.SingleTreeScenarioInjector;
 import works.bosk.testing.drivers.DriverConformanceTest;
+import works.bosk.testing.drivers.state.TestEntity;
 
-// TODO: This currently doesn't test much beyond the driver's ability to pass updates downstream
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @InjectFrom(SingleTreeScenarioInjector.class)
 class JsonNodeDriverConformanceTest extends DriverConformanceTest {
+	private JsonNodeDriver jsonNodeDriver;
+
 	@BeforeEach
 	void setUp() {
-		driverFactory = JsonNodeDriver.factory(new JacksonSerializer());
+		driverFactory = (b,d) -> {
+			BoskDriver build = JsonNodeDriver.<TestEntity>factory(new JacksonSerializer()).build(b, d);
+			jsonNodeDriver = (JsonNodeDriver) build;
+			return build;
+		};
+	}
+
+	@Override
+	protected void assertCorrectBoskContents() {
+		super.assertCorrectBoskContents();
+		JsonNode expected, actual;
+		try (var _ = bosk.readSession()) {
+			var root = bosk.rootReference().value();
+			expected = jsonNodeDriver.mapper.convertValue(root, JsonNode.class);
+			actual = jsonNodeDriver.currentRoot;
+		}
+		assertEquals(expected, actual);
 	}
 }
